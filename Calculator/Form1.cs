43365d90bcb8a8f8b8b0c985d9533cfa777ca5ba
DevDuck33 <game.dev@op.pl>
2019-01-29 21:40:19 +0100

    Basic calculator.

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,9 +12,76 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        Double Value = 0;
+        String Operation = "";
+        bool Operation_Pressed = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if((Result.Text == "0")||(Operation_Pressed))
+            {
+                Result.Clear();
+            }
+            Operation_Pressed = false;
+            Button Button = (Button)sender;
+            Result.Text = Result.Text + Button.Text;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Result.Text = "0";
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button Button = (Button)sender;
+            Operation = Button.Text;
+            Value = Double.Parse(Result.Text);
+            Operation_Pressed = true;
+            equation.Text = Value + " " + Operation;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            equation.Text = "";
+            switch (Operation)
+            {
+                case "+":
+                    {
+                        Result.Text = (Value + Double.Parse(Result.Text)).ToString();
+                        break;
+                    }
+                case "-":
+                    {
+                        Result.Text = (Value - Double.Parse(Result.Text)).ToString();
+                        break;
+                    }
+                case "/":
+                    {
+                        Result.Text = (Value / Double.Parse(Result.Text)).ToString();
+                        break;
+                    }
+                case "*":
+                    {
+                        Result.Text = (Value * Double.Parse(Result.Text)).ToString();
+                        break;
+                    }
+                default:
+                    break;
+            }
+            Operation_Pressed = false;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Result.Text = "0";
+            equation.Text = "";
+            Value = 0;
         }
     }
 }
